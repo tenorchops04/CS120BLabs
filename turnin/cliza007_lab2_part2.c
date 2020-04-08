8c4ccb434d18665f2781b4a47bf695a25ae04c46
Celvin Lizama Pena <cliza007@wch136-04.cs.ucr.edu>
2020-04-08 00:21:56 -0700

    Completed part 2

---
+++ turnin/cliza007_lab2_part2.c
@@ -0,0 +1,39 @@
+/*	Author: cliza007
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs
+       				   // Initialize output on PORTC to 0x00
+
+	unsigned char tempA = 0x00;
+
+	while(1) {
+	tempA = PINA & 0x0F;
+	unsigned char i;
+	unsigned char cntavail = 0x00;
+
+	for(i = 0; i < 4; i++){
+		if((tempA & 0x01) == 0x00){
+			cntavail++;
+		}
+		tempA /= 2;
+	}
+	
+	PORTC = cntavail;	// Writes port C's 8 pins with the values
+				// on port C's 8 pins
+
+	}
+	return 1;
+}
