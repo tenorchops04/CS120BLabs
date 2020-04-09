c044ef5ad942eab1900cb6e4c58bbc6c6fcdbead
Celvin Lizama Pena <cliza007@ucr.edu>
2020-04-09 12:29:50 -0700

    Done with lab

--- turnin/cliza007_lab2_part1.c
+++ turnin/cliza007_lab2_part1.c
@@ -1,7 +1,7 @@
-/*	Author: cliza007
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+/*	Author: Celvin Lizama Pena
+ *  Partner(s) Name: none
+ *	Lab Section: 022
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
--- turnin/cliza007_lab2_part3.c
+++ turnin/cliza007_lab2_part3.c
@@ -1,7 +1,7 @@
-/*	Author: cliza007
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+/*	Author: Celvin Lizama Pena cliza007@ucr.edu
+ *  Partner(s) Name: none
+ *	Lab Section:22
+ *	Assignment: Lab #2  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
