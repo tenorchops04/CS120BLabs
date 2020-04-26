117732fb48c97768085f66e793bbe451b6709275
tenorchops04 <33356170+tenorchops04@users.noreply.github.com>
2020-04-25 18:52:25 -0700

    Update cliza007_lab5_part3.c

--- lab5/turnin/cliza007_lab5_part3.c
+++ lab5/turnin/cliza007_lab5_part3.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Demo: https://drive.google.com/file/d/1r-2HPrLls5jp2Xch5c2uF2vNcq6sWHIq/view?usp=sharing
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
