aea9886353f8c4fc9dbf9f5c54eff40e69aaf282
tenorchops04 <33356170+tenorchops04@users.noreply.github.com>
2020-05-09 23:41:55 -0700

    Update cliza007_lab7_part2.c

--- lab7/turnin/cliza007_lab7_part2.c
+++ lab7/turnin/cliza007_lab7_part2.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo link: https://drive.google.com/file/d/1VZ2hRcpZnNEkkz7DTGFyLKGmL8V4UZGM/view?usp=drivesdk
+ *	Demo link: 
  */
 #include <avr/io.h>
 #include "io.h"
