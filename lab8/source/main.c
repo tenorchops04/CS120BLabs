0ef813bef3ea83f5f423fd45c849f137d3636009
Celvin Lizama Pena <cliza007@ucr.edu>
2020-05-11 16:52:06 -0700

    Started exercise 1

--- lab8/source/main.c
+++ lab8/source/main.c
@@ -1,7 +1,7 @@
-/*	Author: lab
+/*	Author: Celvin Lizama Pena
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #8  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,11 +13,14 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+	/* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
 
-    /* Insert your solution below */
-    while (1) {
+	/* Insert your solution below */
+	while (1) {
 
-    }
-    return 1;
+	}
+	return 1;
 }
