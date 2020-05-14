dc684ac4a0c30772d82d54447b855de57e9ca767
Celvin Lizama Pena <cliza007@ucr.edu>
2020-05-13 17:03:04 -0700

    Implemented part1 logic. Still some bugs to fix.

--- lab8/source/main.c
+++ lab8/source/main.c
@@ -12,15 +12,35 @@
 #include "simAVRHeader.h"
 #endif
 
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: setting this bit enables analog-to-digital conversion.
+	// ADSC: setting this bit starts the first conversion.
+	// ADATE: setting this bit enables auto-triggerin. Since we are 
+	// 	  in Free Running mode, a new conversion will trigger whenever
+	// 	  the previous conversion completes.
+}
+
 int main(void) {
 	/* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRD = 0xFF; PORTD = 0x00;
+	DDRA = 0x00; PORTA = 0xFF; // PORTA set to input.
+	DDRB = 0xFF; PORTB = 0x00; // PORTB set to output.
+	DDRD = 0xFF; PORTD = 0x00; // PORTD set to output.
 
-	/* Insert your solution below */
+	ADC_init();
+	unsigned short my_adc = 0x00;
+	unsigned char tempB = 0x00;
+	unsigned char tempD = 0x00;
+
 	while (1) {
+		my_adc = ADC;
 
+		tempB = (char)my_adc;
+
+		tempD = (char)(my_adc >> 8);
+
+		PORTB = tempB;
+		PORTD = tempD;
 	}
 	return 1;
 }
