c96521c8784eec040e1cad41ee5797c79d1bc3cd
tenorchops04 <33356170+tenorchops04@users.noreply.github.com>
2020-05-16 15:52:52 -0700

    Update cliza007_lab8_part1.c

--- lab8/turnin/cliza007_lab8_part1.c
+++ lab8/turnin/cliza007_lab8_part1.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo link: 
+ *	Demo link: https://drive.google.com/file/d/1t6Vr7BER-ivYk8Oa7JJzTrkaCy99hH5x/view?usp=drivesdk
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
